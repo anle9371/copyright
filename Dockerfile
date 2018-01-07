aadc4a146d61a9e05ef6de527e3260f36fcb80ab
Amy Le <anle9371@colorado.edu>
2018-01-07 15:38:00 +0000

    pip requirements

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,11 @@ FROM gcr.io/tensorflow/tensorflow:latest-devel
 # Copy the current directory contents into the container at /app
 COPY amy* /app/
 
+COPY *.ipynb /app/
+
+# Set the working directory to /app
+WORKDIR /app
+
 RUN export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)" && \
     echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list && \
     curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - && \
@@ -16,7 +21,10 @@ RUN export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)" && \
     apt-get update && apt-get install -y \
     gcsfuse && \
     chmod +x /app/amy.sh
-    
+
+
+RUN pip install --trusted-host pypi.python.org -r requirements.txt
+
 ENTRYPOINT ["/app/amy.sh"]
 
 #EXPOSE 80
--- amy.sh
+++ amy.sh
@@ -4,19 +4,19 @@
 mkdir -p /mnt/gcs
 gcsfuse copyright /mnt/gcs
 
-# Use signals to communicate with an application from the host machine
+# # Use signals to communicate with an application from the host machine
 
 
-# build the model - compile the code (can take five to ten minutes)
-bazel build -c opt --copt=-mavx /tensorflow/tensorflow/examples/image_retraining:retrain
+# # build the model - compile the code (can take five to ten minutes)
+# bazel build -c opt --copt=-mavx /tensorflow/tensorflow/examples/image_retraining:retrain
 
-# retrain with the flower images (around twenty minutes in total)
-bazel-bin/tensorflow/examples/image_retraining/retrain \
-    --bottleneck_dir=/tf_files/bottlenecks \
-    --model_dir=/tf_files/inception \
-    --output_graph=/tf_files/retrained_graph.pb \
-    --output_labels=/tf_files/retrained_labels.txt \
-    --image_dir /tf_files/flower_photos
+# # retrain with the flower images (around twenty minutes in total)
+# bazel-bin/tensorflow/examples/image_retraining/retrain \
+#     --bottleneck_dir=/tf_files/bottlenecks \
+#     --model_dir=/tf_files/inception \
+#     --output_graph=/tf_files/retrained_graph.pb \
+#     --output_labels=/tf_files/retrained_labels.txt \
+#     --image_dir /tf_files/flower_photos
 
-#jupyter notebook --allow-root "$@"
-sleep infinity
+jupyter notebook --allow-root "$@"
+# sleep infinity
