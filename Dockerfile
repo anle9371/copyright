c56150f7d15fb6e64245413c2289a67b6311c537
amy <antuongle@kpmg.com>
2018-01-24 12:41:44 +0100

    adding config file

--- Dockerfile
+++ Dockerfile
@@ -3,12 +3,13 @@ FROM gcr.io/tensorflow/tensorflow:latest-devel
 # Copy the current directory contents into the container at /app
 COPY myapp/* /app/
 
-RUN export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)" && \
-    echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list && \
-    curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - && \
-    apt-get update && apt-get install -y \
-    apt-utils \
-    google-cloud-sdk
+## these RUN commands set up the docker with google cloud services (sdk and storgage)
+# RUN export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)" && \
+#     echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list && \
+#     curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - && \
+#     apt-get update && apt-get install -y \
+#     apt-utils \
+#     google-cloud-sdk
 
 # gcsfuse - connect to google cloud service
 # also see the corresponding lines in amy.sh
@@ -18,15 +19,15 @@ RUN export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)" && \
 #     curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - && \
 #     apt-get update && apt-get install -y \
 #     gcsfuse
+
+RUN apt-get update && apt-get install -y \
+    python-tk
     
 RUN chmod +x /app/amy.sh
 
 RUN pip install --trusted-host pypi.python.org -r /app/requirements.txt
 
-# Set the working directory to /mnt
-WORKDIR /mnt
+# Set the working directory 
+# WORKDIR /tensorflow
 
 ENTRYPOINT ["/app/amy.sh"]
-
-
-
--- myapp/amy.sh
+++ myapp/amy.sh
@@ -6,43 +6,48 @@
 IMGPATH="/mnt/copyright"
 LOGFILE="/app/log.txt"
 
+# create logfile
+touch $LOGFILE
+echo "logfile created" >> $LOGFILE
+
 # # gcsfuse to connect to google cloud storage
 # mkdir -p /mnt/gcs
 # gcsfuse copyright /mnt/gcs
 
 # copy jupyter config file 
-mv /app/jupyter_notebook_config.json ~/.jupyter/
+#mv /app/jupyter_notebook_config.json ~/.jupyter/
+#echo "jupyter config transferred" >> $LOGFILE
 
 # copy demo notebook
 mv /app/using_inception.ipynb /mnt
+echo "demo notebook moved to /mnt" >> $LOGFILE
 
 # extract the images
-cd app
-tar xvf /app/not_sources.tar.gz
-tar xvf /app/sources.tar.gz
+tar xvf /app/not_sources.tar.gz -C /app
+tar xvf /app/sources.tar.gz -C /app
+echo "images untarred" >> $LOGFILE
 
 # create input for the model
 mkdir /mnt/copyright
 python /app/creating_input.py -i /app -o $IMGPATH
+echo "created input for inception" >> $LOGFILE
 
-# create logfile
-touch $LOGFILE
+# # # build the model - compile the code (can take 2hrs)
+# cd /tensorflow
+# bazel build -c opt --copt=-mavx /tensorflow/tensorflow/examples/image_retraining:retrain >> $LOGFILE
 
-# # build the model - compile the code (can take 2hrs)
-cd /tensorflow
-bazel build -c opt --copt=-mavx /tensorflow/tensorflow/examples/image_retraining:retrain >> $LOGFILE
+# echo "\n\n model has been built \n\n" >> $LOGFILE
 
-echo "\n\n model has been built \n\n" >> $LOGFILE
+# # retrain with my own images (around 2hrs in total)
+# bazel-bin/tensorflow/examples/image_retraining/retrain \
+#     --bottleneck_dir=/mnt/bottlenecks \
+#     --model_dir=/mnt/inception \
+#     --output_graph=/mnt/retrained_graph.pb \
+#     --output_labels=/mnt/retrained_labels.txt \
+#     --image_dir $IMGPATH >> $LOGFILE
 
-# retrain with my own images (around 2hrs in total)
-bazel-bin/tensorflow/examples/image_retraining/retrain \
-    --bottleneck_dir=/mnt/bottlenecks \
-    --model_dir=/mnt/inception \
-    --output_graph=/mnt/retrained_graph.pb \
-    --output_labels=/mnt/retrained_labels.txt \
-    --image_dir $IMGPATH >> $LOGFILE
+# echo "\n\n model has been retrained \n\n" >> $LOGFILE
 
-echo "\n\n model has been retrained \n\n" >> $LOGFILE
-
-jupyter notebook --allow-root "$@"
+#jupyter notebook --allow-root --config /app/jupyter_notebook_config.json "$@"
+jupyter notebook --allow-root --no-browser "$@"
 # sleep infinity
